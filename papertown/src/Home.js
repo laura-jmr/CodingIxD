10f06cb3df191482c3a6b6eafbc03e905198efdf
laura-jmr <laura.juergensmeier@gmail.com>
2025-01-18 12:21:25 +0100

    feat: started with home layout

--- papertown/src/Home.js
+++ papertown/src/Home.js
@@ -1,11 +1,33 @@
 import "./Home.css";
 
 function Home() {
-  return (
-    <div id="home">
-
-    </div>
-  );
+    return (
+        <div id="home">
+            <nav>
+                <div>
+                    <p>1 km/h</p>
+                    <p>17:00</p>
+                </div>
+                <div className="input-field">
+                    Your City Name
+                </div>
+                <div>
+                    <div className="input-field">
+                        1000 €
+                    </div>
+                    <div className="input-field">
+                        0 Person
+                    </div>
+                </div>
+            </nav>
+            <div className="input-field">
+                City Score
+            </div>
+            <div className="input-field">
+                Start Simulation
+            </div>
+        </div>
+    );
 }
 
 export default Home;
